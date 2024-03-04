386cfebc44b686993afa9caf8c400ccb37a49450
Manuel Borrero <borreromanuel95@gmail.com>
2024-03-04 22:19:13 +0100

    -Modifica la struttura del progetto -aggiunti i file specifici dei css -aggiunta la pagina delle domande

---
+++ js/index.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded",()=>{
+
+    document.getElementById("btn-proceed").addEventListener("click",()=>{
+
+        window.location.href ="question.html"
+
+    });
+});
+
