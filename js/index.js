bb1685303a16ee3c9fff0514f0ddaa4fbe5d5f2f
Manuel Borrero <borreromanuel95@gmail.com>
2024-03-07 15:10:56 +0100

    fix path to release on server

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("proceedBtn").addEventListener("click", () => {
  
         if (document.getElementById("promiseCheck").checked) {
-            window.location.href = "../question.html";
+            window.location.href = "./question.html";
             console.log("Vai alla question");
         } else {
             document.getElementsByClassName("msg-error")[0].style.display = "block";
